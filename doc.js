8ec55d9e58df36f2c9cf213b18d5b3a51bfaa531
Vig25nesh <mullaguruvignesh@gmail.com>
2024-04-13 15:26:10 -0400

    Made some changes to so that alert would pop up

--- doc.js
+++ doc.js
@@ -1,9 +1,10 @@
 // Selects form element
 const form = document.querySelector("form");
+// "Listens" for users submission once they submit form
 form.addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevents the default form submission behavior
+    event.preventDefault(); 
 
-    // Retrieves the values of the name, email, phone, and message fields from the form
+    // Retrieves the values of input field from the form
     const nameValue = document.querySelector("#name").value;
     const emailValue = document.querySelector("#email").value;
     const phoneValue = document.querySelector("#phone").value;
@@ -11,8 +12,8 @@ form.addEventListener("submit", function(event) {
 
     // Validation Logic
     if (!nameValue || !emailValue || !phoneValue || !messageValue) {
-        alert("All fields are incomplete."); // Alert
+        alert("You didn't fill out evrything yet!"); // Alert
     } else {
-        alert("Thank you for submitting the form. "); // Confirmation Message
+        alert("Thanks for submitting!"); // Confirmation Message
     }
 });
